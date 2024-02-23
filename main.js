775db9c030a2bfa09caa36d96c3fb70124e873d0
mwenning243 <144702170+mwenning243@users.noreply.github.com>
2024-02-23 17:06:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from './three';
+import { GLTFLoader } from './three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from './three/addons/controls/OrbitControls.js';
 
 /*
 
